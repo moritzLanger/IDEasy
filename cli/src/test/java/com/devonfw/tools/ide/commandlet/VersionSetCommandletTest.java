f3e31fedb0eaa6f76dbfd9a34fe7cb0d674c9a96
moritzLanger <44358321+moritzLanger@users.noreply.github.com>
2023-12-04 10:21:34 +0100

    Update VersionSetCommandletTest.java

--- cli/src/test/java/com/devonfw/tools/ide/commandlet/VersionSetCommandletTest.java
+++ cli/src/test/java/com/devonfw/tools/ide/commandlet/VersionSetCommandletTest.java
@@ -13,37 +13,5 @@ import com.devonfw.tools.ide.context.IdeContext;
  */
 public class VersionSetCommandletTest extends AbstractIdeContextTest {
 
-  /**
-   * Test of {@link VersionSetCommandlet} run.
-   *
-   * @throws IOException on error.
-   */
-  @Test
-  public void testVersionSetCommandletRun() throws IOException {
 
-    // arrange
-    String path = "workspaces/foo-test/my-git-repo";
-    IdeContext context = newContext("basic", path, true);
-    VersionSetCommandlet versionSet = context.getCommandletManager().getCommandlet(VersionSetCommandlet.class);
-    versionSet.tool.setValueAsString("mvn");
-    versionSet.version.setValueAsString("3.1.0");
-    // act
-    versionSet.run();
-    // assert
-    Path settingsIdeProperties = context.getSettingsPath().resolve("ide.properties");
-    assertThat(settingsIdeProperties).hasContent("""
-        #********************************************************************************
-        # This file contains project specific environment variables
-        #********************************************************************************
-
-        JAVA_VERSION=17*
-        MVN_VERSION=3.1.0
-        ECLIPSE_VERSION=2023-03
-        INTELLIJ_EDITION=ultimate
-
-        IDE_TOOLS=mvn,eclipse
-
-        BAR=bar-${SOME}
-                """);
-  }
 }
